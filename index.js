64c5cd0dde57d3793fde0661ce93c361db1261e0
blvpeppa <blvpeppgram@gmail>
2025-04-06 11:03:05 +0200

    git commit -m

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port,()=> {
     console.log(`Server is running on port http://localhost:${port}/`);
 });
+app.post('/contact.html',(req,res)=>{
+    res.send("<h1>message successfully delivered</h1>");
+})
